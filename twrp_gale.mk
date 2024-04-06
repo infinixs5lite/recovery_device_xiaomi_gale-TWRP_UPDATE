4dcdcfe39e761245ffbdefa0c21b459d3f65aacf
infinixs5lite <kelvinchinedu97@gmail.com>
2024-04-06 04:17:36 +0100

    Update twrp_gale.mk

--- twrp_gale.mk
+++ twrp_gale.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from gale device
 $(call inherit-product, device/xiaomi/gale/device.mk)
@@ -21,7 +21,7 @@ $(call inherit-product, device/xiaomi/gale/device.mk)
 PRODUCT_DEVICE := gale
 PRODUCT_NAME := twrp_gale
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 13C/Poco C64
+PRODUCT_MODEL := Redmi 13C
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_RELEASE_NAME := gale
 
