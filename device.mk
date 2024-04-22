e4dc0f0af3b39d1a2a7dde0ae4130a323491c945
infinixs5lite <kelvinchinedu97@gmail.com>
2024-04-22 20:12:26 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -64,9 +64,11 @@ PRODUCT_PACKAGES += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
     libkeymaster41 \
-    libpuresoftkeymasterdevice 
+    libpuresoftkeymasterdevice\
+    libfs_mgr
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so 
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libfs_mgr.so
